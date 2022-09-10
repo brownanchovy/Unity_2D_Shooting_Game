2c00446a150cc3bcf979696f10aa0bb727724232
brownanchovy <gogodo1022@gmail.com>
2022-09-10 11:28:28 +0900

    Enemy_Commit

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -8,15 +8,19 @@ public class Enemy : MonoBehaviour
     Rigidbody2D rigid;
     public CircleCollider2D collision;
     SpriteRenderer spriteRenderer;
+    public Sprite[] sprites;
     public float enemyHealth;
+    public float speed;
+    public int health;
+
     // Start is called before the first frame update
     void Awake()
     {
         anim = GetComponent<Animator>();
         spriteRenderer = GetComponent<SpriteRenderer>();
         rigid = GetComponent<Rigidbody2D>();
-        collision = GetComponent<CircleCollider2D>();
-
+        rigid.velocity = Vector2.down * speed;
+        //collision = GetComponent<CircleCollider2D>();
     }
     void Start()
     {
@@ -32,5 +36,35 @@ public class Enemy : MonoBehaviour
     void FixedUpdate()
     {
         
+    }
+
+    void OnHit(int dmg)
+    {
+        health -= dmg;
+        spriteRenderer.sprite = sprites[1];
+        Invoke("ReturnSprite", 0.1f);
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void ReturSprite()
+    {
+        spriteRenderer.sprite = sprites[0];
+    }
+
+    void OnTriggerEnter2D(Collider2D collision) 
+    {
+        if(collision.gameObject.tag == "BorderBullet")
+        {
+            Destroy(gameObject);
+        }
+        else if(collision.gameObject.tag == "PlayerBullet")
+        {
+            Bullet bullet = collision.gameObject.GetComponent<Bullet>();
+            OnHit(bullet.dmg);
+            Destroy(gameObject);
+        }
     }
 }
